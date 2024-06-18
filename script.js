9d05312a901833a046ee6d33ba5ca334549a6934
alonemanuel <alon@alonemanuel.com>
2024-06-18 14:17:25 +0300

    Add menu on top

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let menuFab = document.getElementById("menu-fab");
+let menuButtons = document.getElementById("menu-portrait");
 console.log(menuFab);
 menuFab.addEventListener("click", () => {
-  let menuButtons = document.getElementById("menu-portrait");
   menuButtons.classList.toggle("clicked");
 });
 
-
-let windowInnerHeight = window.innerHeight;
-let windowOuterHeight = window.outerHeight;
-console.log(`inner: ${innerHeight}`);
-console.log(`outer: ${outerHeight}`);
-
-menuFab.style.bottom = '';
-menuFab.style.top = `${windowInnerHeight}px`;
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    menuFab.style.opacity = 1;
+  } else {
+    menuFab.style.opacity = 0;
+    menuButtons.classList.remove("clicked");
+  }
+});
